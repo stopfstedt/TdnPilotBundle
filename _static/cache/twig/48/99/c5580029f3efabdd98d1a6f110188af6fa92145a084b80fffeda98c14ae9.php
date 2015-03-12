4e60cfdc0d8afa11dc02e3e50c40a9440355461b
Victor Passapera <vpassapera@gmail.com>
2015-03-11 22:29:22 -0700

    Publishing docs.

---
+++ _static/cache/twig/48/99/c5580029f3efabdd98d1a6f110188af6fa92145a084b80fffeda98c14ae9.php
@@ -0,0 +1,112 @@
+<?php
+
+/* traits.twig */
+class __TwigTemplate_4899c5580029f3efabdd98d1a6f110188af6fa92145a084b80fffeda98c14ae9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("layout/layout.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'body_class' => array($this, 'block_body_class'),
+            'page_content' => array($this, 'block_page_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "layout/layout.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        $context["__internal_50f291185c8f862c85d3e4ae9b77f6c609e51683b13900b61fe607d08f6c9980"] = $this->env->loadTemplate("macros.twig");
+        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Traits | ";
+        $this->displayParentBlock("title", $context, $blocks);
+    }
+
+    // line 4
+    public function block_body_class($context, array $blocks = array())
+    {
+        echo "traits";
+    }
+
+    // line 6
+    public function block_page_content($context, array $blocks = array())
+    {
+        // line 7
+        echo "    <div class=\"page-header\">
+        <h1>Traits</h1>
+    </div>
+
+    <div class=\"container-fluid underlined\">
+        ";
+        // line 12
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["classes"]) ? $context["classes"] : $this->getContext($context, "classes")));
+        foreach ($context['_seq'] as $context["_key"] => $context["class"]) {
+            // line 13
+            echo "            ";
+            if ($this->getAttribute($context["class"], "trait", array())) {
+                // line 14
+                echo "                <div class=\"row\">
+                    <div class=\"col-md-6\">
+                        ";
+                // line 16
+                echo $context["__internal_50f291185c8f862c85d3e4ae9b77f6c609e51683b13900b61fe607d08f6c9980"]->getclass_link($context["class"], true);
+                echo "
+                    </div>
+                    <div class=\"col-md-6\">
+                        ";
+                // line 19
+                echo $this->env->getExtension('sami')->parseDesc($context, $this->getAttribute($context["class"], "shortdesc", array()), $context["class"]);
+                echo "
+                    </div>
+                </div>
+            ";
+            }
+            // line 23
+            echo "        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['class'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 24
+        echo "    </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "traits.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  94 => 24,  88 => 23,  81 => 19,  75 => 16,  71 => 14,  68 => 13,  64 => 12,  57 => 7,  54 => 6,  48 => 4,  41 => 3,  37 => 1,  35 => 2,  11 => 1,);
+    }
+}
